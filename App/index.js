27add7839017a15e70eddfadc206ecdb391fa8a9
Pieter Herman <identity@pieterherman.dev>
2020-11-26 02:20:22 +0100

    Multi-Theme Button

--- App/index.js
+++ App/index.js
@@ -26,37 +26,41 @@ export default class App extends React.Component {
         <SafeAreaView>
           <Text style={styles.value}>75</Text>
           <Row>
-            <Button text="C" onPress={() => alert('TODO')} />
-            <Button text="+/-" onPress={() => alert('TODO')} />
-            <Button text="%" onPress={() => alert('TODO')} />
-            <Button text="/" onPress={() => alert('TODO')} />
+            <Button text="C" theme="secondary" onPress={() => alert('TODO')} />
+            <Button
+              text="+/-"
+              theme="secondary"
+              onPress={() => alert('TODO')}
+            />
+            <Button text="%" theme="secondary" onPress={() => alert('TODO')} />
+            <Button text="/" theme="accent" onPress={() => alert('TODO')} />
           </Row>
 
           <Row>
             <Button text="7" onPress={() => alert('TODO')} />
             <Button text="8" onPress={() => alert('TODO')} />
             <Button text="9" onPress={() => alert('TODO')} />
-            <Button text="x" onPress={() => alert('TODO')} />
+            <Button text="x" theme="accent" onPress={() => alert('TODO')} />
           </Row>
 
           <Row>
             <Button text="4" onPress={() => alert('TODO')} />
             <Button text="5" onPress={() => alert('TODO')} />
             <Button text="6" onPress={() => alert('TODO')} />
-            <Button text="-" onPress={() => alert('TODO')} />
+            <Button text="-" theme="accent" onPress={() => alert('TODO')} />
           </Row>
 
           <Row>
             <Button text="1" onPress={() => alert('TODO')} />
             <Button text="2" onPress={() => alert('TODO')} />
             <Button text="3" onPress={() => alert('TODO')} />
-            <Button text="+" onPress={() => alert('TODO')} />
+            <Button text="+" theme="accent" onPress={() => alert('TODO')} />
           </Row>
 
           <Row>
             <Button text="0" size="double" onPress={() => alert('TODO')} />
             <Button text="." onPress={() => alert('TODO')} />
-            <Button text="=" onPress={() => alert('TODO')} />
+            <Button text="=" theme="accent" onPress={() => alert('TODO')} />
           </Row>
         </SafeAreaView>
       </View>
