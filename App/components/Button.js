32f0fd1b9e02d1b0adf3a6c74fa0e18fbc77f447
Pieter Herman <identity@pieterherman.dev>
2020-11-26 01:13:58 +0100

    Rough Calculator Layout

---
+++ App/components/Button.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#fff',
+    fontSize: 25,
+  },
+  button: {
+    backgroundColor: '#333333',
+    flex: 1,
+  },
+});
+
+export default ({ onPress, text }) => (
+  <TouchableOpacity onPress={onPress} style={styles.button}>
+    <Text style={styles.text}>{text}</Text>
+  </TouchableOpacity>
+);
